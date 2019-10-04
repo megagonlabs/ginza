f5493f51508769cc5b75996a3dce2cdbbc918158
hiroshi <hiroshi_matsuda@megagon.ai>
2019-10-04 18:01:27 +0900

    improve packaging script

--- shell/package_ginza.sh
+++ shell/package_ginza.sh
@@ -3,7 +3,8 @@ mkdir target
 set -eu
 lang_name=ja
 model_dir=${lang_name}_ginza-$1
-python -m spacy package ${@:2} --force models/${model_dir} target/
+python -m ginza_util.edit_meta_json meta.json.ginza models/${model_dir}/meta.json
+python -m spacy package ${@:2} -m models/${model_dir}/meta.json --force models/${model_dir} target/
 set -e
 
 cd target/${model_dir}/
