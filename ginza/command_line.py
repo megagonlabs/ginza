ad59669e8eea09968df1f62066c855ea44faacf0
r-terada <r.terada1993@gmail.com>
2021-10-14 02:49:05 +0900

    refactor: cut process in run() into funcitons

--- ginza/command_line.py
+++ ginza/command_line.py
@@ -2,7 +2,7 @@
 from multiprocessing import Pool, cpu_count
 from pathlib import Path
 import sys
-from typing import Optional, List
+from typing import Iterable, Optional, List
 
 import plac
 from . import force_using_normalized_form_as_lemma
@@ -79,75 +79,88 @@ def run(
     if parallel <= 0:
         parallel = max(1, cpu_count() + parallel)
 
-    pool = None
-
     output = _OutputWrapper(output_path, output_format)
     output.open()
-
     try:
         if not files:
             if sys.stdin.isatty():
                 parallel = 1
+                _analyze_tty(analyzer, output)
             else:
-                files = [0]
-
-        if not files:
-            analyzer.set_nlp()
-            while True:
-                line = input()
-                for sent in analyzer.analyze_line(line):
-                    for ol in sent:
-                        output.write(ol)
+                _analyze_single(analyzer, output, files=[0])
         elif parallel == 1:
-            analyzer.set_nlp()
-            for path in files:
-                with open(path, "r") as f:
-                    for line in f:
-                        for sent in analyzer.analyze_line(line):
-                            for ol in sent:
-                                output.write(ol)
+            _analyze_single(analyzer, output, files)
         else:
-            buffer = []
-            for file_idx, path in enumerate(files):
-                with open(path, "r") as f:
-                    while True:
-                        eof, buffer = fill_buffer(f, MINI_BATCH_SIZE * parallel, buffer)
-                        if eof and (file_idx + 1 < len(files) or len(buffer) == 0):
-                            break  # continue to next file
-                        if not pool:
-                            if len(buffer) <= MINI_BATCH_SIZE:  # enough for single process
-                                analyzer.set_nlp()
-                                for line in buffer:
-                                    for sent in analyzer.analyze_line(line):
-                                        for ol in sent:
-                                            output.write(ol)
-                                break  # continue to next file
-                            parallel = (len(buffer) - 1) // MINI_BATCH_SIZE + 1
-                            pool = Pool(parallel)
+            _analyze_parallel(analyzer, output, files, parallel)
+    finally:
+        output.close()
 
-                        mini_batch_size = (len(buffer) - 1) // parallel + 1
-                        mini_batches = [
-                            buffer[idx * mini_batch_size : (idx + 1) * mini_batch_size] for idx in range(parallel)
-                        ]
-                        for mini_batch_result in pool.map(analyzer.analyze_lines_mp, mini_batches):
-                            for sents in mini_batch_result:
-                                for lines in sents:
-                                    for ol in lines:
-                                        output.write(ol)
 
-                        buffer.clear()  # process remaining part of current file
-
+def _analyze_tty(analyzer: Analyzer, output: _OutputWrapper) -> None:
+    try:
+        analyzer.set_nlp()
+        while True:
+            line = input()
+            for sent in analyzer.analyze_line(line):
+                for ol in sent:
+                    output.write(ol)
     except EOFError:
         pass
     except KeyboardInterrupt:
         pass
-    finally:
-        try:
-            if pool:
-                pool.close()
-        finally:
-            output.close()
 
+
+def _analyze_single(analyzer: Analyzer, output: _OutputWrapper, files: Iterable[str]) -> None:
+    try:
+        analyzer.set_nlp()
+        for path in files:
+            with open(path, "r") as f:
+                for line in f:
+                    for sent in analyzer.analyze_line(line):
+                        for ol in sent:
+                            output.write(ol)
+    except KeyboardInterrupt:
+        pass
+
+
+def _analyze_parallel(analyzer: Analyzer, output: _OutputWrapper, files: Iterable[str], parallel: int) -> None:
+    pool = None
+    buffer = []
+    try:
+        for file_idx, path in enumerate(files):
+            with open(path, "r") as f:
+                while True:
+                    eof, buffer = fill_buffer(f, MINI_BATCH_SIZE * parallel, buffer)
+                    if eof and (file_idx + 1 < len(files) or len(buffer) == 0):
+                        break  # continue to next file
+                    if not pool:
+                        if len(buffer) <= MINI_BATCH_SIZE:  # enough for single process
+                            analyzer.set_nlp()
+                            for line in buffer:
+                                for sent in analyzer.analyze_line(line):
+                                    for ol in sent:
+                                        output.write(ol)
+                            break  # continue to next file
+                        parallel = (len(buffer) - 1) // MINI_BATCH_SIZE + 1
+                        pool = Pool(parallel)
+                    mini_batch_size = (len(buffer) - 1) // parallel + 1
+                    mini_batches = [
+                        buffer[idx * mini_batch_size : (idx + 1) * mini_batch_size] for idx in range(parallel)
+                    ]
+                    for mini_batch_result in pool.map(analyzer.analyze_lines_mp, mini_batches):
+                        for sents in mini_batch_result:
+                            for lines in sents:
+                                for ol in lines:
+                                    output.write(ol)
+                    buffer.clear()  # process remaining part of current file
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if pool:
+            try:
+                pool.close()
+            except:
+                pass
 
 def fill_buffer(f, batch_size, buffer=None):
     if buffer is None:
