1a95324d7b2429389d9f1b8af964e53e3522aec3
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2021-12-06 00:03:22 +0900

    update dependencies

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires=[
         "spacy>=3.2.0,<3.3.0",
         "plac>=1.3.3",
-        "SudachiPy>=0.6.0,<0.7.0",
+        "SudachiPy>=0.5.4,<0.7.0",
         "SudachiDict-core>=20210802",
     ],
     setup_requires=["pytest-runner"],
